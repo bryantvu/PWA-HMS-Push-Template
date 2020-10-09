51411687e06cbc78293502e2aa7e254b911fc614
bryantvu <bryantvu@stepsmobile.com>
2020-10-09 08:01:10 -0700

    first commit

---
+++ app/src/main/java/com/bvutest/pwa/push/pwa/MainActivity.java
@@ -0,0 +1,212 @@
+package com.bvutest.pwa.push.pwa;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.webkit.*;
+
+import com.huawei.agconnect.config.AGConnectServicesConfig;
+import com.huawei.hmf.tasks.OnCompleteListener;
+import com.huawei.hmf.tasks.OnFailureListener;
+import com.huawei.hmf.tasks.OnSuccessListener;
+import com.huawei.hmf.tasks.Task;
+import com.huawei.hms.aaid.HmsInstanceId;
+import com.huawei.hms.aaid.entity.AAIDResult;
+import com.huawei.hms.common.ApiException;
+import com.huawei.hms.push.HmsMessaging;
+
+import org.json.*;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        loadManifest();
+        setDisplay(this);
+        setOrientation(this);
+        setName(this);
+        setContentView(R.layout.activity_main);
+        setWebView((WebView) this.findViewById(R.id.webView));
+        //getAAID();
+        getToken();
+        addTopic();
+    }
+
+    private void setDisplay(Activity activity) {
+        if (this.manifestObject.optString("display").equals("fullscreen")) {
+            activity.setTheme(R.style.FullscreenTheme);
+        } else {
+            activity.setTheme(R.style.AppTheme);
+        }
+    }
+
+    private void setName(Activity activity) {
+        String name = this.manifestObject.optString("name");
+        if (!name.isEmpty()) {
+            activity.setTitle(name);
+        }
+    }
+
+    private static final String ANY = "any";
+    private static final String NATURAL = "natural";
+    private static final String PORTRAIT_PRIMARY = "portrait-primary";
+    private static final String PORTRAIT_SECONDARY = "portrait-secondary";
+    private static final String LANDSCAPE_PRIMARY = "landscape-primary";
+    private static final String LANDSCAPE_SECONDARY = "landscape-secondary";
+    private static final String PORTRAIT = "portrait";
+    private static final String LANDSCAPE = "landscape";
+
+    private void setOrientation(Activity activity) {
+        String orientation = this.manifestObject.optString("orientation");
+        switch (orientation) {
+            case LANDSCAPE_PRIMARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                break;
+            case PORTRAIT_PRIMARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                break;
+            case LANDSCAPE:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE);
+                break;
+            case PORTRAIT:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT);
+                break;
+            case LANDSCAPE_SECONDARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+                break;
+            case PORTRAIT_SECONDARY:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT);
+                break;
+            case ANY:
+            case NATURAL:
+            default:
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+                break;
+        }
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private void setWebView(WebView myWebView) {
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        String start_url = this.manifestObject.optString("start_url");
+        String scope = this.manifestObject.optString("scope");
+        myWebView.setWebViewClient(new PwaWebViewClient(start_url, scope));
+        myWebView.loadUrl(start_url);
+    }
+
+    private static final String DEFAULT_MANIFEST_FILE = "manifest.json";
+    private JSONObject manifestObject;
+
+    private void loadManifest() {
+        if (this.assetExists((DEFAULT_MANIFEST_FILE))) {
+            try {
+                this.manifestObject = this.loadLocalManifest();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean assetExists(String asset) {
+        final AssetManager assetManager = this.getResources().getAssets();
+        try {
+            return Arrays.asList(assetManager.list("")).contains(asset);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    private JSONObject loadLocalManifest() throws JSONException {
+        try {
+            InputStream inputStream = this.getResources().getAssets().open(DEFAULT_MANIFEST_FILE);
+            int size = inputStream.available();
+            byte[] bytes = new byte[size];
+            int readBytes = inputStream.read(bytes);
+            inputStream.close();
+            if (readBytes > 0) {
+                String jsonString = new String(bytes, "UTF-8");
+                return new JSONObject(jsonString);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private void getAAID() {
+        Task<AAIDResult> idResult = HmsInstanceId.getInstance(this).getAAID();
+        idResult.addOnSuccessListener(new OnSuccessListener<AAIDResult>() {
+            @Override
+            public void onSuccess(AAIDResult aaidResult) {
+                String aaId = aaidResult.getId();
+                Log.d(TAG,"getAAID success:" + aaId);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(Exception e) {
+                Log.e(TAG, "getAAID failed:" + e);
+            }
+        });
+
+    }
+
+    private void getToken() {
+        Log.d(TAG, "getToken:begin");
+
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    // read from agconnect-services.json
+//                    String appId = "102916931";
+                    String appId = AGConnectServicesConfig.fromContext(MainActivity.this).getString("client/app_id");
+                    String token = HmsInstanceId.getInstance(MainActivity.this).getToken(appId, "HCM");
+                    Log.i(TAG, "get token:" + token);
+                    if(!TextUtils.isEmpty(token)) {
+                        Log.i(TAG, "sending token to server. token:" + token);
+                    }
+
+                } catch (ApiException e) {
+                    Log.e(TAG, "get token failed, " + e);
+                }
+            }
+        }.start();
+    }
+
+    private void addTopic() {
+        try {
+            HmsMessaging.getInstance(MainActivity.this)
+                    .subscribe("msg")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Log.i(TAG, "subscribe Complete");
+                            }else{
+                                Log.i(TAG, "subscribe failed");
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            Log.e("subscribe failed",  e.getMessage());
+        }
+    }
+
+
+}
